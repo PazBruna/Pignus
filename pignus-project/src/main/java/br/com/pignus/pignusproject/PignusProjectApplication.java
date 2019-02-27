afc8e4dfe2d430e018cec3f5891ac3d973bd3621
Guilherme <guilhermeraulino2704@gmail.com>
2019-02-27 18:43:59 -0300

    Projeto SpringBoot

---
+++ pignus-project/src/main/java/br/com/pignus/pignusproject/PignusProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.pignus.pignusproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PignusProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PignusProjectApplication.class, args);
+	}
+
+}
---
+++ pignus-project/src/test/java/br/com/pignus/pignusproject/PignusProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.pignus.pignusproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PignusProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
