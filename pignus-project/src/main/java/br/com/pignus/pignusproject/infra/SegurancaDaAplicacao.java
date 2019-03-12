98413dbe895e994a7a14b275059d137b8038f5fa
Guigo Faria <guilhermeraulino2704@gmail.com>
2019-03-12 11:44:33 -0300

    Separação dos dados em outra classe

--- pignus-project/src/main/java/br/com/pignus/pignusproject/infra/SegurancaDaAplicacao.java
+++ pignus-project/src/main/java/br/com/pignus/pignusproject/infra/SegurancaDaAplicacao.java
@@ -2,30 +2,22 @@ package br.com.pignus.pignusproject.infra;
 
 import org.springframework.stereotype.Component;
 
+import br.com.pignus.pignusproject.repository.DadosUsuarios;
+
 @Component
 public class SegurancaDaAplicacao {
+	DadosUsuarios dados = new DadosUsuarios();
+	
+	
 
 	public Boolean permitirAcesso(String email, String senha) {
+		String matrizLogin[][] = dados.retornaMatriz();
 		boolean resposta = false;
-		String matrizLogin[][] = new String[5][2];
-		matrizLogin[0][0] = "teste@teste";
-		matrizLogin[0][1] = "1234";
-
-		matrizLogin[1][0] = "gui@teste";
-		matrizLogin[1][1] = "4321";
-
-		matrizLogin[2][0] = "bianca@teste";
-		matrizLogin[2][1] = "eminem";
-
-		matrizLogin[3][0] = "eu@meu";
-		matrizLogin[3][1] = "nos";
-
-		matrizLogin[4][0] = "gui@teste";
-		matrizLogin[4][1] = "4321";
-
+		boolean respostaEmail = false;
+		boolean respostaSenha = false;
+		
 		for (int i = 0; i < matrizLogin.length; i++) {
-			boolean respostaEmail = false;
-			boolean respostaSenha = false;
+			
 			if (matrizLogin[i][0].equals(email)) {
 				respostaEmail = true;
 			}
