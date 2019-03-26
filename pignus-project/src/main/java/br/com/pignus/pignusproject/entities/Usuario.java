41473e36aaae789763d4e4d22b1e1d45c8021b0a
Guilherme Raulino <guilhermeraulino2704@gmail.com>
2019-03-26 13:23:43 -0300

    Tentativa de configuração SQLServer

--- pignus-project/src/main/java/br/com/pignus/pignusproject/entities/Usuario.java
+++ pignus-project/src/main/java/br/com/pignus/pignusproject/entities/Usuario.java
@@ -17,59 +17,61 @@ import javax.persistence.InheritanceType;
 @DiscriminatorValue("C")
 public class Usuario {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String email;
 	private String senha;
 	private String nome;
 	private int setor;
-	@Column(insertable=false, updatable=false)
+	@Column(insertable = false, updatable = false)
 	private String tipo;
-	
-	
-	
-	
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
 	public int getSetor() {
 		return setor;
 	}
+
 	public void setSetor(int setor) {
 		this.setor = setor;
 	}
-	
-	
-	
-	
 
 }
