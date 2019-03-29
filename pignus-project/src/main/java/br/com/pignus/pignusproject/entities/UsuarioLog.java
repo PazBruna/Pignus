7c9240dc3c45b787388c74fcd161be04a933e495
Bianca Gomes Fenelon de Moraes <biafenelon2014@outlook.com>
2019-03-29 14:05:14 -0300

    criando classe de LOG

---
+++ pignus-project/src/main/java/br/com/pignus/pignusproject/entities/UsuarioLog.java
@@ -0,0 +1,45 @@
+package br.com.pignus.pignusproject.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class UsuarioLog {
+	
+	@Id
+	@GeneratedValue
+	private int logId;
+	@ManyToOne
+	private Usuario usuario;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date usuarioData;
+	
+	
+	public int getLogId() {
+		return logId;
+	}
+	public void setLogId(int logId) {
+		this.logId = logId;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Date getUsuarioData() {
+		return usuarioData;
+	}
+	public void setUsuarioData(Date usuarioData) {
+		this.usuarioData = usuarioData;
+	}
+	
+	
+}
