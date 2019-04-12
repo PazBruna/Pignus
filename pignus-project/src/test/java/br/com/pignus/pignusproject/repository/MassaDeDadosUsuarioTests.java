27a2afdc0488ef1fc44f6693bc3716e0f3df23c6
David Neri <davidneri@MacBook-Pro-de-David.local>
2019-04-12 17:05:29 -0300

    MassaDeDados

---
+++ pignus-project/src/test/java/br/com/pignus/pignusproject/repository/MassaDeDadosUsuarioTests.java
@@ -0,0 +1,18 @@
+package br.com.pignus.pignusproject.repository;
+
+import org.junit.Test;
+
+import br.com.pignus.pignusproject.entities.UsuarioGestor;
+
+public class MassaDeDadosUsuarioTests {
+	UsuarioGestor gestores = new UsuarioGestor();
+	
+	  
+	
+	
+	@Test 
+	public void MassaDeDados (){
+		
+	}
+
+}
