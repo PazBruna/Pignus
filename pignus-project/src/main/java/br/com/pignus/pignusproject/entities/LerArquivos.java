c77e90f6372f749c4a746b65fd9c7eab87af3d8e
Bruno Carvalho <brunopapamike18@gmail.com>
2019-05-09 20:55:38 -0300

    salve bianca

--- pignus-project/src/main/java/br/com/pignus/pignusproject/entities/LerArquivos.java
+++ pignus-project/src/main/java/br/com/pignus/pignusproject/entities/LerArquivos.java
@@ -1,100 +1,63 @@
 package br.com.pignus.pignusproject.entities;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.PrintStream;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 
 public class LerArquivos
 {
-   public static void main( String[] args ) throws FileNotFoundException
+   public static void main( String[] args ) 
    {
-//       Scanner sc= null;
-//        
-//       /* Abre o arquivo */
-//       try
-//       {
-//          sc = new Scanner(new FileReader("C:\\Users\\Bruno Carvalho\\Desktop\\salve.txt")).useDelimiter(";|\\r\\n");
-//       }
-//       catch ( FileNotFoundException fileNotFoundException )
-//       {
-//          System.err.println( "Erro ao abrir arquivo." );
-//          System.exit( 1 );
-//       }
-//
-//       Usuario disc= new Usuario();
-//       
-//       /* Exibe os titulos das colunas */
-//       System.out.printf( "%-14s%-30s%-12s%-12s\n", "email","senha","nome","funcao");
-//      
-//       /* Tenta ler e exibir os dados do arquivo até encontrar o fim de arquivo */
-//       
-//       try // le dados do arquivo usando o Scanner
-//       {
-//          while ( sc.hasNext() )
-//          {
-//        	  disc.setNome( sc.next()); 
-//              disc.setSenha( sc.next()); 
-//              disc.setFuncao( sc.next()); 
-//              disc.setEmail( sc.next()); 
-//             
-//              
-//              // exibe os dados
-//        	  System.out.printf( "%-14s%-30s%-12s%-12s\n", disc.getNome(),
-//             		 disc.getSenha(), disc.getFuncao(), disc.getEmail());
-//          }
-//       }
-//       catch ( NoSuchElementException elementException )
-//       {
-//           System.err.println( "Arquivo com problemas." );
-//           sc.close();
-//           System.exit( 1 );
-//       }
-//       catch ( IllegalStateException stateException )
-//       {
-//           System.err.println( "Erro na leitura do arquivo." );
-//           System.exit( 1 );
-//       }
-// 
-//       /* Fecha o arquivo */
-//       if ( sc != null )
-//           sc.close();
+       Scanner sc= null;
+        
+       /* Abre o arquivo */
+       try
+       {
+          sc = new Scanner(new FileReader("testeCsv.csv")).useDelimiter(";|\\r\\n");
+       }
+       catch ( FileNotFoundException fileNotFoundException )
+       {
+          System.err.println( "Erro ao abrir arquivo." );
+          System.exit( 1 );
+       }
+
+       Download disc= new Download();
        
-       
-       //////////////////////////////////
-       
-       
+       /* Exibe os titulos das colunas */
+       System.out.printf( "%-14s%-30s%-12s\n", "nomePrograma","categoria","link");
       
-   		BufferedReader file2 = new BufferedReader(new FileReader("testeCsv.csv"));    
-   		try {
-   			   
-   			
-   				while(file2.ready()){
-   					   String line = file2.readLine();
-   					   System.out.println(line);				   
-   					}
-   		}
-   		 catch (Exception e) {
-   			System.out.println("Erro na leitura: "+e);
-   		}
-   	
-   	
-   	
+       /* Tenta ler e exibir os dados do arquivo até encontrar o fim de arquivo */
        
-       
-       
-       
-       
-       
-       
-       
-       
-       
-   
+       try // le dados do arquivo usando o Scanner
+       {
+          while ( sc.hasNext() )
+          {
+        	  disc.setNomePrograma( sc.next()); 
+              disc.setCategoria( sc.next()); 
+              disc.setLink( sc.next()); 
+             
+              // exibe os dados
+        	  System.out.printf( "%-14s%-30s%-12s\n", disc.getNomePrograma(),
+             		 disc.getCategoria(), disc.getLink());
+          }
+       }
+       catch ( NoSuchElementException elementException )
+       {
+           System.err.println( "Arquivo com problemas." );
+           sc.close();
+           System.exit( 1 );
+       }
+       catch ( IllegalStateException stateException )
+       {
+           System.err.println( "Erro na leitura do arquivo." );
+           System.exit( 1 );
+       }
+ 
+       /* Fecha o arquivo */
+       if ( sc != null )
+           sc.close();
    }
 }
-
+       
