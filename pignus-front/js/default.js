e6df9bba3a2703d7d559c46060c37d83e9563c26
Diego Braga <diego.braga@digisystem.com.br>
2019-05-13 10:59:42 -0300

    Estruturação Inicial do SCSS

---
+++ pignus-front/js/default.js
@@ -0,0 +1,48 @@
+/*
+ * VARIÁVEIS DE CORES DOS INTEGRANTES
+ */
+
+var anaBeatriz_1 = 'f7e4ad';
+var anaBeatriz_2 = 'ffbb00';
+var anaBeatriz_3 = 'b28302';
+
+var biancaGomes_1 = 'c2f3fc';
+var biancaGomes_2 = '6fd5e9';
+var biancaGomes_3 = '2092a8';
+
+var brunaPaz_1 = 'fc9aae';
+var brunaPaz_2 = 'eb1d47';
+var brunaPaz_3 = '960220';
+
+var brunoCarvalho_1 = '9c9c9c';
+var brunoCarvalho_2 = '424242';
+var brunoCarvalho_3 = '121212';
+
+var diegoBraga_1 = 'a7fac6';
+var diegoBraga_2 = '00ff5f';
+var diegoBraga_3 = '016e2a';
+
+var guilhermeRaulino_1 = 'c4a8f7';
+var guilhermeRaulino_2 = '391f68';
+var guilhermeRaulino_3 = '0c021f';
+
+/* 
+ * VARIÁVEIS DE CORES DA EMPRESA
+ */
+
+var pigRed_1 = 'eb1d47';
+var pigRed_2 = '990820';
+var pigRed_3 = '5C0315';
+
+var pigBlue_1 = '';
+var pigBlue_2 = '';
+var pigBlue_3 = '';
+
+/*
+ * VARIÁVEIS DE ESTRUTURA DO MENU
+ */
+
+var menu_1 = '040505'
+var menu_2 = '131314';
+var background_1 = '373737';
+var background_2 = 'e8e8e8'
