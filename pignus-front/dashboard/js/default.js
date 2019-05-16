a509f5966a9ea0362371230826113ce14774ee90
Diego Braga <diego.braga@digisystem.com.br>
2019-05-16 11:29:29 -0300

    Finalização da Construção do Menu

--- pignus-front/dashboard/js/default.js
+++ pignus-front/dashboard/js/default.js
@@ -46,3 +46,37 @@ var menu_1 = '040505'
 var menu_2 = '131314';
 var background_1 = '373737';
 var background_2 = 'e8e8e8'
+
+var menuElement = document.getElementById('header-dashboard');
+var screenWidth = screen.width;
+var containerDashboard = document.getElementById('body-dashboard')
+var closeElement = document.getElementById('close-menu');
+var openElement = document.getElementById('open-menu');
+var contentMenuElement = document.getElementById('header-content');
+
+openElement.onclick = function() {  
+  containerDashboard.style.overflowY = 'hidden';
+  menuElement.style.display = 'block';
+  closeElement.style.display = 'block';
+  
+  if (screenWidth <= '768') {
+    menuElement.style.width = '88%';
+  } else {
+    menuElement.style.width = '342px';
+  }
+  
+  setTimeout(function() {
+    contentMenuElement.style.visibility = 'visible';
+  }, 50)
+}
+
+closeElement.onclick = function() {
+  containerDashboard.style.overflowY = 'scroll';
+  contentMenuElement.style.visibility = 'hidden';
+  closeElement.style.display = 'none';
+  
+  setTimeout(function() {
+    menuElement.style.width = '0';
+    menuElement.style.display = 'none';
+  }, 50)
+}
