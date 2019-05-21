f860757d2005e81ee44bd9088cb0dd54c6437fe3
Guigo Faria <guilhermeraulino2704@gmail.com>
2019-05-21 11:30:32 -0300

    Downloads cadastrados e mostrando na página

--- pignus-project/src/main/java/br/com/pignus/pignusproject/controller/DownloadsController.java
+++ pignus-project/src/main/java/br/com/pignus/pignusproject/controller/DownloadsController.java
@@ -3,6 +3,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import br.com.pignus.pignusproject.entities.Download;
 import br.com.pignus.pignusproject.entities.LerArquivos;
 import br.com.pignus.pignusproject.repository.DownloadRepository;
+import br.com.pignus.pignusproject.repository.FuncaoRepository;
 
 @Controller
 public class DownloadsController {
@@ -18,19 +20,23 @@ public class DownloadsController {
 	
 	@Autowired
 	private DownloadRepository meusDownloads;
+	@Autowired
+	private FuncaoRepository funcoes;
 
-	@PostMapping("/meusDownloads")
-	public String salvandoDownloads (){
-		return PAGINA_DE_DOWNLOADS;
+
+	@PostMapping("/downloads")
+	public String salvarDownloads() {
+		LerArquivos ler = new LerArquivos();
+		if(ler.listaDownloads()== null) {
+			System.out.println("Deu ruim");
+		}
+		meusDownloads.saveAll(ler.listaDownloads());
+		return "redirect:meusDownloads";
 	}
 	
 	@RequestMapping(value = "/meusDownloads", method=RequestMethod.GET)
 	public String pegandoArquivoLista (@ModelAttribute Download download, Model model) {
-		LerArquivos ler = new LerArquivos();
-		if(ler.listaDownloads() == null) {
-			System.out.println("Deu ruim");
-		}
-		meusDownloads.saveAll(ler.listaDownloads());
+		
 		List<Download> listaArqv = meusDownloads.findAll();
 		model.addAttribute("listaArqv", listaArqv).toString();	
 		return PAGINA_DE_DOWNLOADS;
--- pignus-project/src/main/java/br/com/pignus/pignusproject/entities/LerArquivos.java
+++ pignus-project/src/main/java/br/com/pignus/pignusproject/entities/LerArquivos.java
@@ -16,59 +16,57 @@ import br.com.pignus.pignusproject.repository.FuncaoRepository;
 public class LerArquivos {
 	@Autowired
 	private FuncaoRepository fc;
-	Funcoes funcao = new Funcoes();
-	
-	
+
 	public List<Download> listaDownloads() {
-		{
-			Scanner sc = null;
-			List<Download> lista = new ArrayList<>();
-			
-			/* Abre o arquivo */
-			try {
-				sc = new Scanner(new FileReader("download.csv")).useDelimiter(";|\\r\\n");
-			} catch (FileNotFoundException fileNotFoundException) {
-				System.err.println("Erro ao abrir arquivo.");
-				System.exit(1);
-			}
 
-			/* Exibe os titulos das colunas */
-			System.out.printf("%-14s%-30s%-12s%-12s\n", "nomePrograma", "categoria", "link","id");
+		Scanner sc = null;
+		List<Download> lista = new ArrayList<>();
 
-			/* Tenta ler e exibir os dados do arquivo até encontrar o fim de arquivo */
-
-			try // le dados do arquivo usando o Scanner
-			{
-				
-				while (sc.hasNext()) {
-					Download disc = new Download();
-					disc.setNomePrograma(sc.next());
-					disc.setLink(sc.next());
-					funcao.setId(sc.nextInt());
-					Funcoes novaFuncao =fc.getOne(funcao.getId());
-				
-					disc.setFuncaoDownload(novaFuncao);
-					lista.add(disc);
-					
-					
-				}
-				return lista;
-
-			} catch (NoSuchElementException elementException) {
-				System.err.println("Arquivo com problemas.");
-				sc.close();
-				System.exit(1);
-			} catch (IllegalStateException stateException) {
-				System.err.println("Erro na leitura do arquivo.");
-				System.exit(1);
-			}
-
-			/* Fecha o arquivo */
-			if (sc != null) {
-				sc.close();
-			}
-			
+		/* Abre o arquivo */
+		try {
+			sc = new Scanner(new FileReader("download.csv")).useDelimiter(";|\\r\\n");
+		} catch (FileNotFoundException fileNotFoundException) {
+			System.err.println("Erro ao abrir arquivo.");
+			System.exit(1);
 		}
-		return null;
+
+		/* Exibe os titulos das colunas */
+		System.out.printf("%-14s%-12s%-12s\n", "nomePrograma", "link", "idFuncao");
+
+		/* Tenta ler e exibir os dados do arquivo até encontrar o fim de arquivo */
+
+		try // le dados do arquivo usando o Scanner
+		{
+
+			while (sc.hasNext()) {
+				Download download = new Download();
+				
+				download.setNomePrograma(sc.next());
+				
+				download.setLink(sc.next());
+				
+				int id = sc.nextInt();
+				Funcoes funcao = new Funcoes();
+				funcao.setId(id);
+				download.setFuncaoDownload(funcao);
+
+				lista.add(download);
+			}
+
+		} catch (NoSuchElementException elementException) {
+			System.err.println("Arquivo com problemas.");
+			sc.close();
+			System.exit(1);
+		} catch (IllegalStateException stateException) {
+			System.err.println("Erro na leitura do arquivo.");
+			System.exit(1);
+		}
+
+		/* Fecha o arquivo */
+		if (sc != null) {
+			sc.close();
+		}
+		return lista;
 	}
+
 }
