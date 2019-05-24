55d731a643451659b4dbc1559b3a1d627c891fad
isilva-igs <silvaigs.is@gmail.com>
2019-05-23 21:10:07 -0300

    linkandando paginas com o back

--- pignus-project/src/main/java/br/com/pignus/pignusproject/controller/GerenciamentoRotasPaginas.java
+++ pignus-project/src/main/java/br/com/pignus/pignusproject/controller/GerenciamentoRotasPaginas.java
@@ -7,11 +7,51 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller 
 public class GerenciamentoRotasPaginas {
+	
+	
 	@GetMapping("/home")
 	public String paginaPrincipal(HttpSession session) {
 		session.getAttribute("usuarioLogado");
 		
 		return "dashboard/dashboard";
 	}
-
+	
+	@GetMapping("/meusProjetos")
+	public String meusProjetos(HttpSession session) {
+		session.getAttribute("usuarioLogado");
+		
+		return "dashboard/meus-projetos";
+	}
+	
+	@GetMapping("/novoPrograma")
+	public String novoPrograma(HttpSession session) {
+		session.getAttribute("usuarioLogado");
+		
+		return "dashboard/novo-programa";
+	}
+	
+	@GetMapping("/novoProjeto")
+	public String novoprojeto(HttpSession session) {
+		session.getAttribute("usuarioLogado");
+		
+		return "dashboard/novo-projeto";
+	}
+	
+	@GetMapping("/novoSetor")
+	public String novoSetor(HttpSession session) {
+		session.getAttribute("usuarioLogado");
+		
+		return "dashboard/novo-setor";
+	}
+	
+	@GetMapping("/configuracoesUsuario")
+	public String configuracoesUsuario(HttpSession session) {
+		session.getAttribute("usuarioLogado");
+		
+		return "dashboard/configuracoes-usuario";
+	}
+	
+	
 }
+
+
